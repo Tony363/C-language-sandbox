d574786d11e7d861850662823dff3ff834cf86d1
tony363 <pysolver33@gmail.com>
2020-12-28 21:55:19 +0800

    FUCK ME IT WORKS

--- Clang/Integral_approximations/rule_approx.c
+++ Clang/Integral_approximations/rule_approx.c
@@ -3,51 +3,75 @@
 
 //https://stackoverflow.com/questions/21146540/integrate_by_trapezoidal_rule-rule-in-python
 
-double fx(double x){
-    return x*x;
-}
 
-double integrate_by_trapezoidal_rule(double (*f)(double),double Ubound,double Lbound,double n){
-    double range = (Lbound - Ubound) / n;
-    double area = 0.0;
-    area += range * f(Ubound)/2.0;
-    for(int i=1;i<n;i++){
-        area += f(Ubound + i*range);
+
+double integrate_by_trapezoidal_rule(const char *expr,double Ubound,double Lbound,double iter){
+    double range = (Lbound - Ubound) / iter,area = 0.0,x,y,e;
+
+    te_variable var[] = {{"x",&x},{"y",&y},{"e",&e}};
+
+    int err;
+    te_expr *n = te_compile(expr, var, 3, &err);
+    if (n){
+        x = Ubound;
+        const double r = te_eval(n);
+
+        area += range * r/2.0;
+        for(int i=1;i<iter;i++){
+            x = (Ubound + i*range);
+            te_expr *n = te_compile(expr,var,3,&err);
+            const double r = te_eval(n);
+            area += r;
+        }
+
+        x = Lbound;
+        te_expr *n = te_compile(expr,var,3,&err);
+        const double final = te_eval(n);
+
+        area += final/2.0;
+        
+        te_free(n);
+    }else {
+        printf("\t%*s^\nError near here", err-1, "");
+        te_free(n);
     }
-    area += f(Lbound)/2.0;
     return area * range;
 }
 
-double integrate_by_midpont(double (*fx)(double),double Ubound,double Lbound,double n){
-    double range = (Lbound - Ubound)/n;
-    double area = 0.0;
-    for (int i=0;i<n;i++){
-        area += fx((Ubound + range/2.0)+i*range);
+double integrate_by_midpont(const char *expr,double Ubound,double Lbound,double iter){
+    double range = (Lbound - Ubound) / iter,area = 0.0,x,y,e;
+
+    te_variable var[] = {{"x",&x},{"y",&y},{"e",&e}};
+    int err;
+
+    for (int i=0;i<iter;i++){
+        x = (Ubound + range/2.0)+i*range;
+        te_expr *n = te_compile(expr,var,3,&err);
+        if(!n){
+            printf("\t%*s^\nError near here", err-1, "");
+        }
+        const double r = te_eval(n);
+        area += r;
+        te_free(n);
     }
     area *= range;
     return area;
 }
 
-int main (void){
-    char *str;
-    const char *expr = fgets(str,10,stdin);
-    printf("Evaluating:\n\t%s\n",expr);
-    
-    double x,y,e;
-    te_variable var[] = {{"x",&x},{"y",&y},{"e",&e}};
-    
-    int err;
-    te_expr *n = te_compile(expr,var,3,&err);
-    
-    if (n){
-        x = scanf("Enter [X]: %lf\n",&x);
-        y = scanf("Enter [Y]: %lf\n",&y);
-    } else{
-        printf("\t%*s^\nError near here", err-1, "");
+int main (int argc,char *argv[]){
+
+    if (argc < 2) {
+        printf("Usage: example2 \"expression\"\n");
+        return 0;
     }
-    double area_of_trapezoid = integrate_by_trapezoidal_rule(fx,5,10,100);
-    double area_of_midpoint =  integrate_by_midpont(fx,5,10,100);
+
+    const char *expr = argv[1];
+    printf("Evaluating:\n\t%s\n", expr);
+
+    double area_of_trapezoid = integrate_by_trapezoidal_rule(expr,5,10,100);
+    double area_of_midpoint =  integrate_by_midpont(expr,5,10,100);
     printf("%lf\n",area_of_trapezoid);
     printf("%lf\n",area_of_midpoint);
+
     return 0;
 }
