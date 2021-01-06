dff84428039230a5db43f05f57d6de12157cdfe4
tony363 <pysolver33@gmail.com>
2021-01-06 19:38:53 +0800

    numpy dot product implemented

--- Clang/numpy_array/dynamic_matrix.c
+++ Clang/numpy_array/dynamic_matrix.c
@@ -9,6 +9,10 @@ typedef struct IntMat{
     void *data;
 } IntMat;
 
+typedef struct MatMulVal{
+    int value;
+} MatMulVal;
+
 void *createMat(int rows,int cols){
     IntMat *m = (IntMat *)malloc(sizeof(IntMat));
     m->type = '0';
@@ -70,14 +74,35 @@ void printMatrix(IntMat *matrix){
 
 }
 
+IntMat *copy(IntMat *mat){
+    IntMat *m = createMat(mat->rows,mat->cols);
+    for (int i=0;i<mat->rows;i++){
+        for (int j=0;j<mat->cols;j++){
+            //printf("Enter value for matrix[%d][%d]:\n",i,j);scanf("%d",&input);
+            set(m,i,j,*(((int *)mat->data)+(i*(mat->cols))+j));
+        }
+    }
+    return m;
+}
+
 IntMat *dotProduct(IntMat *matrix,IntMat *vector){
-    IntMat *m = createMat(matrix->rows,matrix->cols);
-    if (matrix->cols == vector->rows){
-        for (int i=0;i<m->rows;i++){
-            for (int j=0;j<m->cols;j++){
-                // printf("%d\n",(*(((int *)matrix->data)+(i*(matrix->cols))+j))*(*(((int *)vector->data)+(i*(vector->cols))+j)));
-                set(m,i,j,(*(((int *)matrix->data)+(i*(matrix->cols))+j))*(*(((int *)vector->data)+(i*(vector->cols))+j)));
+    IntMat *m = createMat(matrix->rows,vector->cols);
+    if (matrix->rows == vector->cols || matrix->cols == vector->rows){
+        for (int i=0,j=0;i<matrix->rows,j<vector->cols;i++,j++){
+            // i iterates matrix rows
+            // j interates vectors cols
+            int value=0,m_val,v_val;
+            for (int m_iter=0,v_iter=0;m_iter<matrix->cols,v_iter<vector->rows;m_iter++,v_iter++){
+                // m_iter is number columns for each row in input matrix
+                // v_iter is number rows in column in vector matrix
+                m_val = (*(((int *)matrix->data)+(i*(matrix->cols))+(m_iter)));
+                v_val = (*(((int *)vector->data)+(j*(vector->rows))+v_iter));
+                value += (m_val * v_val);
+                // Debug outputs
+                // printf("m:v = %d:%d\n", m_val, v_val);
+                // printf("value: %d\n",m_val * v_val);
             }
+            set(m,i,j,value);
         }
         m->result = 'S';
         return m;
@@ -100,7 +125,7 @@ int main(void){
         }
     }
     matrix->result = 'S';
-    IntMat *matrixCopy = matrix;
+    IntMat *matrixCopy = copy(matrix);
     printMatrix(matrix);
     transMat(matrix);
     printMatrix(matrix);
