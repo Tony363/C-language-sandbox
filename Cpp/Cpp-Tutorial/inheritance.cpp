793f4ec8680e403212923e025f4a009f6f2c0af5
pysolver33 <pysolver33@gmail.com>
2021-02-26 21:15:55 +0800

    FUCK THE IMPLICICITY OF LIFE

--- Cpp/Cpp-Tutorial/inheritance.cpp
+++ Cpp/Cpp-Tutorial/inheritance.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class Point;
+class Complex;
+ostream& operator<<(ostream& out, const Complex& c);
+ostream& operator<<(ostream& out, const Point& c);
+
 class Point{
-    // private:
-        // int x,y;
+    private:
+        int x,y;
     public:
-    int x,y;
+    // int x,y;
         Point(){
             x = 0; y = 0;
         }
@@ -24,34 +29,41 @@ class Point{
         int getY() const { return y; }
 };
 
-ostream& operator<<(ostream& out, const Point& c){
-    out << "Point: " << c.getX() << endl;
-    out << "Point: " << c.getY() << endl;
-    return out;
-}
-
 class Complex: public Point{
     // private:
     //     int &real,&imag;
     public:
-        // int &real,&imag;
+        int c1,c2;
         Complex(int r,int i): Point(r,i)//,real(x),imag(y)
         {  
-            cout << "Forming.....\n" << *this << endl;
+            c1 = r;c2 = i;
+            cout << "Forming..." << *this;
         } 
-        // Point& operator=(Point a){
-        //     x = a.x; imag = a.y;
-        //     return *this;
-        // }
+        int getc1()const{return c1;}
+        int getc2()const{return c2;}
 };
+/*
+Forming...x (real): 15 15 y (imag): 30 30
+Forming...x (real): 100 100 y (imag): 200 200
 
+c1: (Point) x:100 y:200
+c2: x:15 y:30
+
+c1: x (real): 100 100 y (imag): 200 100
+c2: x (real): 15 15 y (imag): 30 30
+
+*/
 ostream& operator<<(ostream& out, const Complex& c){
-    out << "Complex x: " << c.getX() << endl;
-    out << "Complex y: " << c.getY() << endl;
+    out << "x (real): " << c.getc1() << " " << c.getc1() << " ";
+    out << "y (imag): " << c.getc2() << " " << c.getc2() << endl;
     return out;
 }
 
-
+ostream& operator<<(ostream& out, const Point& c){
+    out << "x:" << c.getX() << " ";
+    out << "y:" << c.getY() << endl;
+    return out;
+}
 
 // template<typename T>
 void swap( Complex &a,Complex &b){
@@ -62,9 +74,13 @@ void swap( Complex &a,Complex &b){
 
 int main()
 {
-    Complex c1(15, 15), c2 (100, 100);
-    swap(c1,c2);
-    cout << c1;
-    cout << c2;
+    Complex c1(15, 30), c2 (100, 200);
+    Point& p_c1 = c1; Point& p_c2 = c2;
+    cout << endl;
+    swap (p_c1, p_c2);  // swapping Complex objects as Point objects
+    
+    cout << "c1: (Point) " << p_c1 << "c2: " << p_c2 << endl;	    
+    cout << "c1: " << c1 << "c2: " << c2 << endl;
     return 0;
 }
+
