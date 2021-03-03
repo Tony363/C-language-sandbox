790ee07e84df0343a1fe6a9956f8695cdee56d90
pysolver33 <pysolver33@gmail.com>
2021-03-03 18:00:12 +0800

    no shit don't get how jar files work....

--- Cpp/PA_4/FitnessApplication.cpp
+++ Cpp/PA_4/FitnessApplication.cpp
@@ -14,12 +14,12 @@ class readFile;
 
 ifstream& operator>>(ifstream& is, ExercisePlan& plans);
 ifstream& operator>>(ifstream& is, DietPlan& plans);
+ofstream& operator>>(ofstream& os,ExercisePlan& plans);
+ofstream& operator>>(ofstream& os,ExercisePlan& plans);
 
-iostream& operator<<(iostream& out,ExercisePlan& plans);
-iostream& operator<<(iostream& out,DietPlan& plans);
-iostream& operator<<(iostream& out,int& goal);
-
-// void BindInput( ifstream & inputChannel ); 
+ostream& operator<<(ostream& out,ExercisePlan& plans);
+ostream& operator<<(ostream& out,DietPlan& plans);
+ostream& operator<<(ostream& out,int& goal);
 
 int main(){
     FitnessAppWrapper app();
@@ -39,7 +39,9 @@ class Plans{
         string dateIntended; // date plan is intended to be
 
         void editGoal() {
-            cout << "Pleasifstream& operator>>(ifstream& is, plans)e enter goal(calories):" << endl;
+            cout << "Pleas enter goal(calories):\n" 
+                << "-> " 
+                << endl;
             cin >> goalCalories;
         }
 };
@@ -63,6 +65,15 @@ class FitnessAppWrapper{
         ifstream dietfile,exercisefile;
         vector<DietPlan> StoredDietPlans = {};
         vector<ExercisePlan> StoredExercisePlans = {};
+        // enum days {
+        //     Monday="Monday",
+        //     Tuesday="Tuesday", 
+        //     Wednesday="Wednessday",
+        //     Thursday="Thursday",
+        //     Friday="Friday",
+        //     Saturday="Saturday",
+        //     Sunday="Sunday"
+        //     };
 
     public:
         FitnessAppWrapper(){
@@ -76,45 +87,190 @@ class FitnessAppWrapper{
             }
 
         void loadDailyPlan(ifstream &Dietfile,DietPlan &plan){
-            dietfile.open("dietPlans.txt");
-            if (dietfile.is_open()){
-                dietfile >> plan;
+            Dietfile.open("dietPlans.txt");
+            if (Dietfile.is_open()){
+                Dietfile >> plan;
+                // each stored vector object same or different?
+                StoredDietPlans.push_back(plan);
             } else {
                 cout << "Unable to open dietPlans.txt" << endl;
             }
         }
 
         void loadDailyPlan(ifstream &Exercisefile,ExercisePlan &plan){
-            exercisefile.open("exercisePlans.txt");
-            if (exercisefile.is_open()){
-                exercisefile >> plan;
+            Exercisefile.open("exercisePlans.txt");
+            if (Exercisefile.is_open()){
+                Exercisefile >> plan;
+                // each stored vector object same or different?
+                StoredExercisePlans.push_back(plan);
             } else {
                 cout << "Unable to open dietPlans.txt" << endl;
             }
         }
 
-        void loadWeeklyPlan(ifstream &Exercisefile,vector<DietPlan> PlansToStore){
-            class DietPlan forDiet;
+        void loadWeeklyPlan(ifstream &Dietfile){
             for(int i=0;i<7;i++){
-                loadDailyPlan(dietfile,forDiet);
+                DietPlan forDiet;
+                loadDailyPlan(Dietfile,forDiet);
             }
         }
 
-        void loadWeeklyPlan(ifstream &Exercisefile,vector<ExercisePlan> PlansToStore){
-            class ExercisePlan forExercise;
+        void loadWeeklyPlan(ifstream &Exercisefile){
             for(int i=0;i<7;i++){
-                loadDailyPlan(dietfile,forExercise);
+                ExercisePlan forExercise;
+                loadDailyPlan(Exercisefile,forExercise);
             }
         }
 
-        template<class plans>
-        void displayDailyPlan(plans & plan){
+        void displayDailyPlan(ExercisePlan& plan){
             cout << plan;
         }
+
+        void displayDailyPlan(DietPlan& plan){
+            cout << plan;
+        }
+        
+        void displayWeeklyPlan(vector<ExercisePlan> plan){
+            for (auto& it:plan){
+                displayDailyPlan(it);
+            }
+        }
+
+        void displayWeeklyPlan(vector<DietPlan> plan){
+            for (auto& it:plan){
+                displayDailyPlan(it);
+            }
+        }  
+        // ?? does this is even work to not overwrite file of original?
+        void storeDailyPlan(ExercisePlan& plan){
+            ofstream outfile;
+            outfile.open("exerciseStore.txt",ios::out |ios::app);
+            outfile << plan;
+            outfile.close();
+        }
+        // ?? does this is even work to not overwrite file of original?
+        void storeDailyPlan(DietPlan plan){
+            ofstream outfile;
+            outfile.open("dietStore.txt",ios::out |ios::app);
+            outfile << plan;
+            outfile.close();
+        }
+
+        void storeWeeklyPlan(vector<ExercisePlan> plans){
+            for (auto& it:plans){
+                storeDailyPlan(it);
+            }
+        }
+        
+        void storeWeeklyPlan(vector<DietPlan> plans){
+            for (auto& it:plans){
+                storeDailyPlan(it);
+            }
+        }
+        
+        void editDietPlans(){
+            string inputDay;
+            cout << "Please enter plan day to edit:\n"
+            << "-> ";
+            cin >> inputDay; 
+            if (inputDay == "Monday"){
+                StoredDietPlans[0].editGoal();
+            }else if (inputDay == "Tuesday"){
+                StoredDietPlans[1].editGoal();
+            }else if (inputDay == "Wednessday"){
+                StoredDietPlans[2].editGoal();
+            }else if (inputDay == "Thursday"){
+                StoredDietPlans[3].editGoal();
+            }else if (inputDay == "Friday"){
+                StoredDietPlans[4].editGoal();
+            }else if (inputDay == "Saturday"){
+                StoredDietPlans[5].editGoal();
+            }else if (inputDay == "Sunday"){
+                StoredDietPlans[6].editGoal();
+            }else {
+                cout << "Please input day of week with first letter capitalized" 
+                << endl;
+            } 
+        }
+
+        void editExercisePlans(){
+            string inputDay;
+            cout << "Please enter plan day to edit:\n"
+            << "-> ";
+            cin >> inputDay; 
+            if (inputDay == "Monday"){
+                StoredExercisePlans[0].editGoal();
+            }else if (inputDay == "Tuesday"){
+                StoredExercisePlans[1].editGoal();
+            }else if (inputDay == "Wednessday"){
+                StoredExercisePlans[2].editGoal();
+            }else if (inputDay == "Thursday"){
+                StoredExercisePlans[3].editGoal();
+            }else if (inputDay == "Friday"){
+                StoredExercisePlans[4].editGoal();
+            }else if (inputDay == "Saturday"){
+                StoredExercisePlans[5].editGoal();
+            }else if (inputDay == "Sunday"){
+                StoredExercisePlans[6].editGoal();
+            }else {
+                cout << "Please input day of week with first letter capitalized" 
+                << endl;
+            }    
+        }
+
         void closeApp(){
             cout << "closing APP" << endl;
             dietfile.close();
             exercisefile.close();
+        }
+        
+        void displayMenu(){
+            int choice;
+            cout << "Please Choose Options\n"
+                 << "[1] Load weekly diet plan from file\n"
+                 << "[2] Load weekly exercise plan from file\n"
+                 << "[3] Store weekly diet plan to file\n"
+                 << "[4] Store weekly exerise plan to file\n"
+                 << "[5] Display weekly diet plan to screen\n"
+                 << "[6] Display weekly exercise plan to screen\n"
+                 << "[7] Edit daily diet plan\n"
+                 << "[8] Edit daily exercise plan\n"
+                 << "[9] Exit.\n"
+                 << "-> ";
+
+            cin >> choice;
+            evalChoice(choice);
+        }
+
+        void evalChoice(int choice){
+            switch (choice){
+                case 1: 
+                    loadWeeklyPlan(exercisefile);
+                    break;
+                case 2: 
+                    loadWeeklyPlan(dietfile);
+                    break;
+                case 3: 
+                    storeWeeklyPlan(StoredDietPlans);
+                    break;
+                case 4:
+                    storeWeeklyPlan(StoredExercisePlans);
+                    break;
+                case 5: 
+                    displayWeeklyPlan(StoredDietPlans);
+                    break;
+                case 6: 
+                    displayWeeklyPlan(StoredExercisePlans);
+                    break;
+                case 7: 
+                    editDietPlans();
+                    break;
+                case 8: 
+                    editExercisePlans();
+                    break;
+                case 9:
+
+            }
         }
 };  
 
@@ -126,6 +282,13 @@ class readFile{
         string line;
 
     public:
+        template<class plans>
+        readFile(ofstream &file,plans& plan){
+            file << plan.planName;
+            file << plan.goalCalories;
+            file << plan.dateIntended;
+            file << "\n";
+        }
         template<class plans>
         readFile(ifstream& file,plans& plan){
             file.seekg(curPos);
@@ -156,6 +319,7 @@ class readFile{
 
 ifstream& operator>>(ifstream& is, ExercisePlan& plans){
     readFile file(is,plans);
+    // is >> file;
     return is;
 }
 ifstream& operator>>(ifstream& is, DietPlan& plans){
@@ -163,21 +327,30 @@ ifstream& operator>>(ifstream& is, DietPlan& plans){
     return is;
 }
 
-iostream& operator<<(iostream& out,ExercisePlan& plans){
+ofstream& operator<<(ofstream& os,ExercisePlan& plans){
+    readFile file(os,plans);
+    return os;
+}
+ofstream& operator<<(ofstream& os,DietPlan& plans){
+    readFile file(os,plans);
+    return os;
+}
+
+ostream& operator<<(ostream& out,ExercisePlan& plans){
             out << "Plan: " << plans.planName 
-                << "Goals: "<< plans.goalCalories 
+                << "Goals: "<< to_string(plans.goalCalories)
                 << "Date: " << plans.dateIntended 
                 << endl;
             return out;
         }
-iostream& operator<<(iostream& out,DietPlan& plans){
+ostream& operator<<(ostream& out,DietPlan& plans){
             out << "Plan: " << plans.planName 
-                << "Goals: "<< plans.goalCalories 
+                << "Goals: "<< to_string(plans.goalCalories) 
                 << "Date: " << plans.dateIntended 
                 << endl;
             return out;
         }
-iostream& operator<<(iostream& out,int& goal){
-            out << "You goal is updated (" << goal << ")" << endl;
+ostream& operator<<(ostream& out,int& goal){
+            out << "You goal is updated (" << to_string(goal) << ")" << endl;
             return out;
         }
